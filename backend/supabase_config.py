6a4d0ff8f0a9dae692425dcf66e4cd4d16c9f0d3
Amelia <>
2025-08-09 10:27:10 -0700

    fix typo of SUPABASE_API_KEY

--- backend/supabase_config.py
+++ backend/supabase_config.py
@@ -4,10 +4,10 @@ import os
 load_dotenv()
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_API_KET = os.getenv("SUPABASE_API_KEY")
+SUPABASE_API_KEY = os.getenv("SUPABASE_API_KEY")
 
 DEFAULT_HEADERS = {
-  "apikey": SUPABASE_API_KET,
-  "Authorization": f"Bearer {SUPABASE_API_KET}",
+  "apikey": SUPABASE_API_KEY,
+  "Authorization": f"Bearer {SUPABASE_API_KEY}",
   "Content-Type": "application/json"
 }
